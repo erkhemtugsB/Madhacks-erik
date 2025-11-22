ebe798b332489353462712ba2d2a73f519bd2d4d
Chongda You <74991409+chongdayou@users.noreply.github.com>
2025-11-22 15:39:39 -0600

    feat: add Spring Boot structure for file-manage

---
+++ file-manage/src/main/java/com/clear_meet/file_manage/FileManageApplication.java
@@ -0,0 +1,13 @@
+package com.clear_meet.file_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManageApplication.class, args);
+	}
+
+}
